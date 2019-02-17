47e237f4949e823ecd6feaa097453473225c2499
Sergio Baro <sbaro@hubspot.com>
2019-02-17 22:31:15 +0000

    Fixed missing fonts

--- gloomhaven/AppDelegate.swift
+++ gloomhaven/AppDelegate.swift
@@ -6,13 +6,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
+    UINavigationBar.appearance().titleTextAttributes = [.font: UIFont.pirataOne(size: 20.0)]
     UITabBar.appearance().tintColor = .black
     UINavigationBar.appearance().tintColor = .black
     
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = UIStoryboard(name: "Monsters", bundle: nil).instantiateInitialViewController()
-    window?.makeKeyAndVisible()
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = UIStoryboard(name: "Monsters", bundle: nil).instantiateInitialViewController()
+    self.window?.makeKeyAndVisible()
     
     return true
   }
---
+++ gloomhaven/Utils/UIFont+factory.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIFont {
+  
+  static func pirataOne(size: CGFloat) -> UIFont {
+    return UIFont(name: "PirataOne-Regular", size: size)!
+  }
+  
+  static func highTower(size: CGFloat) -> UIFont {
+    return UIFont(name: "HighTowerText-Reg", size: size)!
+  }
+  
+  static func nyala(size: CGFloat) -> UIFont {
+    return UIFont(name: "Nyala", size: size)!
+  }
+  
+}
